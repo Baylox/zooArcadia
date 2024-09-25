2898297bbc93b374825b155a4f30780a50acf72c
Baylospawn <microsoftbaylo@laposte.net>
2024-09-26 00:46:32 +0200

    Ajout Entité Rapport

---
+++ src/Entity/Rapport.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RapportRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RapportRepository::class)]
+class Rapport
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateRapport = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $details = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateRapport(): ?\DateTimeInterface
+    {
+        return $this->dateRapport;
+    }
+
+    public function setDateRapport(\DateTimeInterface $dateRapport): static
+    {
+        $this->dateRapport = $dateRapport;
+
+        return $this;
+    }
+
+    public function getDetails(): ?string
+    {
+        return $this->details;
+    }
+
+    public function setDetails(?string $details): static
+    {
+        $this->details = $details;
+
+        return $this;
+    }
+}
