d0b2ce66eb445a3da51b9e699493211f0f9183d3
Baylospawn <microsoftbaylo@laposte.net>
2024-09-26 00:35:50 +0200

    Ajout Entité Habitat en DQL

---
+++ src/Entity/Habitat.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HabitatRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HabitatRepository::class)]
+class Habitat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $typeHabitat = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTypeHabitat(): ?string
+    {
+        return $this->typeHabitat;
+    }
+
+    public function setTypeHabitat(?string $typeHabitat): static
+    {
+        $this->typeHabitat = $typeHabitat;
+
+        return $this;
+    }
+}
