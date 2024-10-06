b9c6b0138b49d35d9c892f88d19861d89836bc75
Baylospawn <microsoftbaylo@laposte.net>
2024-10-06 17:43:47 +0200

    Refactor: Maj controller dashboard admin et CRUD utilisateur

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Rapport;
+use App\Entity\Utilisateur;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -26,7 +27,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Rapport', 'fa-solid fa-notes-medical', Rapport::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa-solid fa-user-group', Utilisateur::class);
+        yield MenuItem::linkToCrud('Rapports', 'fa-solid fa-notes-medical', Rapport::class);
     }
 }
 
--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -14,15 +15,33 @@ class UtilisateurCrudController extends AbstractCrudController
     {
         return Utilisateur::class;
     }
+    
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs')
 
-    /*
+            ->setPageTitle('index', 'Administration des Utilisateurs')
+
+            ->setPaginatorPageSize(10)
+
+            ->setSearchFields(['id', 'nom', 'prenom', 'email', 'role'])
+            ->setDefaultSort(['id' => 'ASC']);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnForm(),
+            
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            TextField::new('email'),
+            TextField::new('role'),
         ];
-    }
-    */
+    } 
 }
+
+
