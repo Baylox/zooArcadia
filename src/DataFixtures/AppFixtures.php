1f3d9c23983f9d1ab11b1610cd0b53cd89b1eb38
Baylospawn <microsoftbaylo@laposte.net>
2024-10-08 18:07:06 +0200

    Ajout de UtilisateurFactory pour créer des utilisateurs dans AppFixtures pour faciliter les tests

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,15 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Rapport;
+use App\Factory\UtilisateurFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-
+class AppFixtures extends Fixture {
+    public function load(ObjectManager $manager)
+    {
+    // Créer 10 utilisateurs
+    UtilisateurFactory::createMany(10);
+    $manager->flush();
+    }
+}
