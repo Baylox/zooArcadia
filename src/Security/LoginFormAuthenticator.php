3316c6e5d4f691615367f4f8c31661125fa5c9f1
Baylospawn <microsoftbaylo@laposte.net>
2024-10-09 14:15:09 +0200

    Redirection vers la page d'accueil en cas de connexion

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,12 +2,12 @@
 
 namespace App\Security;
 
-
-
 use App\Entity\Utilisateur;
 use App\Repository\UtilisateurRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
@@ -16,19 +16,20 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
-
-
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
     private UtilisateurRepository $utilisateurRepository;
+    private RouterInterface $router;
 
-    public function __construct(UtilisateurRepository $utilisateurRepository)
+    public function __construct(UtilisateurRepository $utilisateurRepository, RouterInterface $router)
     {
         $this->utilisateurRepository = $utilisateurRepository;
+        $this->router = $router;
     }
+
     public function supports(Request $request): ?bool
     {
-        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
+        return ($request->getPathInfo() === '/login' && $request->isMethod('POST'));
     }
 
     public function authenticate(Request $request): Passport
@@ -36,42 +37,32 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         $email = $request->request->get('email');
         $password = $request->request->get('password');
 
-        return new Passport( // On crée l'objet Passport, identifié par l'email et le mot de passe 
+        return new Passport(
             new UserBadge($email, function($userIdentifier) {
-                $utilisateur = $this->utilisateurRepository->findOneBy(['email' => $userIdentifier]); // Si j'utilise cette fonction c'est pour pouvoir implémenter ma propre logique de vérification 
-                
+                $utilisateur = $this->utilisateurRepository->findOneBy(['email' => $userIdentifier]);
+
                 if (!$utilisateur) {
                     throw new UserNotFoundException();
                 }
-                
+
                 return $utilisateur;
             }),
-            
-            // new PasswordCredentials($password) est remplacé par CustomCredentials dans notre cas
-            new CustomCredentials(function($credentials, Utilisateur $utilisateur) { 
-                return $credentials === 'password'; // On test si cela marche
+            new CustomCredentials(function($credentials, Utilisateur $utilisateur) {
+                return $credentials === 'password'; // Logique de test pour les credentials
             }, $password)
         );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        dd('Authentification réussie');
+        return new RedirectResponse(
+            $this->router->generate('app_home') // Redirige vers la page d'accueil après succès
+        );
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         dd('Authentification échouée');
     }
-
-    //    public function start(Request $request, AuthenticationException $authException = null): Response
-    //    {
-    //        /*
-    //         * If you would like this class to control what happens when an anonymous user accesses a
-    //         * protected page (e.g. redirect to /login), uncomment this method and make this class
-    //         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
-    //         *
-    //         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-    //         */
-    //    }
 }
+
