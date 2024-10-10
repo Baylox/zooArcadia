6f7a1cae13785f6b82c3a162105a6641e23cefc8
Baylospawn <microsoftbaylo@laposte.net>
2024-10-10 16:15:37 +0200

    Hashage du password dans les fixtures (Logique de manipulation de fausses données)

--- src/Factory/UtilisateurFactory.php
+++ src/Factory/UtilisateurFactory.php
@@ -4,19 +4,18 @@ namespace App\Factory;
 
 use App\Entity\Utilisateur;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
  * @extends PersistentProxyObjectFactory<Utilisateur>
  */
 final class UtilisateurFactory extends PersistentProxyObjectFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
-    public function __construct()
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
+        $this->passwordHasher = $passwordHasher;
     }
 
     public static function class(): string
@@ -34,19 +33,27 @@ final class UtilisateurFactory extends PersistentProxyObjectFactory
         return [
             'email' => self::faker()->email(),
             'nom' => self::faker()->text(25),
-            'password' => self::faker()->password(),
+            'password' => 'password',
             'prenom' => self::faker()->text(25),
             'roles' => [],
         ];
     }
 
+
     /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     * Initialisation de l'objet Utilisateur après son instanciation.
+     * 
+     * @return static
      */
     protected function initialize(): static
     {
         return $this
-            // ->afterInstantiate(function(Utilisateur $utilisateur): void {})
-        ;
+            ->afterInstantiate(function(Utilisateur $utilisateur): void {
+                // Si un mot de passe est défini, on le hache avant de le sauvegarder
+                if ($utilisateur->getPassword()) {
+                    $hashedPassword = $this->passwordHasher->hashPassword($utilisateur, $utilisateur->getPassword());
+                    $utilisateur->setPassword($hashedPassword);
+                }
+            });
     }
 }
