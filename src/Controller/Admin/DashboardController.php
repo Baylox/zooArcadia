381e0ead01ec2b3d8d83023908a6fc3c1264188f
Baylospawn <microsoftbaylo@laposte.net>
2024-10-15 23:34:26 +0200

    Refactor : Maj des icônes du dashboard admin + Possibilité de regarder les détails des entités

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,14 +5,17 @@ namespace App\Controller\Admin;
 use App\Entity\Espece;
 use App\Entity\Rapport;
 use App\Entity\Utilisateur;
+use App\Entity\Animal;
+use App\Entity\Habitat;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use App\Entity\Animal;
-use App\Entity\Habitat;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -37,9 +40,15 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Animaux', 'fa-solid fa-hippo', Animal::class);
         yield MenuItem::linkToCrud('Habitats', 'fa-solid fa-tree', Habitat::class);
         yield MenuItem::linkToCrud('Especes', 'fa-solid fa-feather-pointed ', Espece::class);
+        yield MenuItem::linkToRoute('Page d\'acceuil', 'fa fa-globe', 'app_home'); 
+    }
+
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 }
-
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
