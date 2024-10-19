a3f4dd6b5fb11751dbdd4faa4703e032a5b6bcd8
Baylospawn <microsoftbaylo@laposte.net>
2024-10-19 15:26:23 +0200

    Refactor: Maj en Français du CRUD

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/animal')]
+#[Route('/dash/animal')]
 final class AnimalController extends AbstractController
 {
     #[Route(name: 'app_animal_index', methods: ['GET'])]
