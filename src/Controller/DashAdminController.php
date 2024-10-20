12fa6dc9dc16968953c1c7a40e7edd25fdb335ad
Baylospawn <microsoftbaylo@laposte.net>
2024-10-21 01:30:13 +0200

    Refactor: Maj DashAdminController et index.html.twig
    - Ajout de l'attribut #[IsGranted('ROLE_ADMIN')] dans DashAdminController
    - Mise à jour de la route dans DashAdminController pour '/dash/admin' et modification du chemin du template vers 'admin/index.html.twig'

--- src/Controller/DashAdminController.php
+++ src/Controller/DashAdminController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashAdminController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/dash/admin', name: 'app_dash_admin')]
     public function index(): Response
     {
-        return $this->render('dash_admin/index.html.twig', [
+        return $this->render('admin/index.html.twig', [
             'controller_name' => 'DashAdminController',
-        ]);
+        ]);  
     }
 }
