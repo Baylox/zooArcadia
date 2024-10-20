f81f2a571a951a32573e09265c4e9aefe57a3fe7
Baylospawn <microsoftbaylo@laposte.net>
2024-10-21 00:43:37 +0200

    Refactor: Majde security.yaml et ajout de DashAdminController et index.html.twig
    Maj de security.yaml pour inclure de nouveaux chemins d'accès contrôlés pour les rôles employe et veterinaire.
    Ajout du fichier DashAdminController.php avec la méthode index().
    Ajout du template index.html.twig pour le tableau de bord de l'utilisateur admin.

---
+++ src/Controller/DashAdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashAdminController extends AbstractController
+{
+    #[Route('/dash/admin', name: 'app_dash_admin')]
+    public function index(): Response
+    {
+        return $this->render('dash_admin/index.html.twig', [
+            'controller_name' => 'DashAdminController',
+        ]);
+    }
+}
