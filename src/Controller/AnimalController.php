a377d6ee41f68161986b78afb8075d3cbbb7db0a
Baylospawn <microsoftbaylo@laposte.net>
2024-10-21 02:40:00 +0200

    Mise à jour des routes pointant vers le dossier dashboard

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/dash/animal')]
 final class AnimalController extends AbstractController
 {
-    #[Route(name: 'app_animal_index', methods: ['GET'])]
+    #[Route(name: 'dashboard_animal_index', methods: ['GET'])]
     public function index(AnimalRepository $animalRepository): Response
     {
         return $this->render('dashboard/animal/index.html.twig', [
@@ -22,7 +22,7 @@ final class AnimalController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_animal_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'dashboard_animal_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $animal = new Animal();
@@ -33,24 +33,24 @@ final class AnimalController extends AbstractController
             $entityManager->persist($animal);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('dashboard_animal_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('animal/new.html.twig', [
+        return $this->render('dashboard/animal/new.html.twig', [
             'animal' => $animal,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_animal_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'dashboard_animal_show', methods: ['GET'])]
     public function show(Animal $animal): Response
     {
-        return $this->render('animal/show.html.twig', [
+        return $this->render('dashboard/animal/show.html.twig', [
             'animal' => $animal,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_animal_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'dashboard_animal_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Animal $animal, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(AnimalType::class, $animal);
@@ -59,16 +59,16 @@ final class AnimalController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('dashboard_animal_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('animal/edit.html.twig', [
+        return $this->render('dashboard/animal/edit.html.twig', [
             'animal' => $animal,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_animal_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'dashboard_animal_delete', methods: ['POST'])]
     public function delete(Request $request, Animal $animal, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$animal->getId(), $request->getPayload()->getString('_token'))) {
@@ -76,6 +76,6 @@ final class AnimalController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('dashboard_animal_index', [], Response::HTTP_SEE_OTHER);
     }
 }
