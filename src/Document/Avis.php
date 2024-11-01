d05cae98e514eac97e711ca10db6d744477f8844
Baylospawn <microsoftbaylo@laposte.net>
2024-11-01 02:35:55 +0100

    Ajout de la classe Avis pour la gestion des avis visiteurs

---
+++ src/Document/Avis.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+#[ODM\Document(collection: 'avis')]
+class Avis
+{
+    #[ODM\Id]
+    private ?string $id = null;
+
+    #[ODM\Field(type: 'string')]
+    private string $pseudo;
+
+    #[ODM\Field(type: 'string')]
+    private string $commentaire;
+
+    #[ODM\Field(type: 'boolean')]
+    private bool $isValide = false;
+
+    // Getters et Setters
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+        return $this;
+    }
+
+    public function isValide(): bool
+    {
+        return $this->isValide;
+    }
+
+    public function setIsValide(bool $isValide): self
+    {
+        $this->isValide = $isValide;
+        return $this;
+    }
+}
+
