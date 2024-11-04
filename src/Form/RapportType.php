77b4c81dabb7c7c98337cd9ccb5fde5043295366
Baylospawn <microsoftbaylo@laposte.net>
2024-11-04 01:21:40 +0100

    Refactor : Mise à jour du formulaire RapportType pour intégrer une relation avec les alimentations et améliorer l'interface utilisateur

--- src/Form/RapportType.php
+++ src/Form/RapportType.php
@@ -9,26 +9,34 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class RapportType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('dateRapport', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('details')
-            ->add('animaux', EntityType::class, [
-                'class' => Animal::class,
-                'choice_label' => 'prenom',
-                'multiple' => true,
-            ])
-            ->add('utilisateur', EntityType::class, [
-                'class' => Utilisateur::class,
-                'choice_label' => 'prenom',
-            ])
+        ->add('titre')
+        ->add('dateRapport', null, [
+            'widget' => 'single_text',
+        ])
+        ->add('animal', EntityType::class, [
+            'class' => Animal::class,
+            'choice_label' => 'prenom',
+            'placeholder' => 'Sélectionnez un animal',
+        ]) 
+        ->add('alimentations', CollectionType::class, [
+            'entry_type' => AlimentationType::class,
+            'entry_options' => ['label' => false],
+            'allow_add' => true,
+            'allow_delete' => true,
+            'by_reference' => false,
+        ])       
+        ->add('details')
+        ->add('utilisateur', EntityType::class, [
+            'class' => Utilisateur::class,
+            'choice_label' => 'prenom',
+        ])
         ;
     }
 
