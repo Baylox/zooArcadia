fb7ebca2243e8739b5f9910b9353020996543306
Baylospawn <microsoftbaylo@laposte.net>
2024-11-07 03:54:29 +0100

    Ajout de la classe Horaire pour gérer les horaires d'ouverture et de fermeture, ainsi que des tests unitaires pour valider l'insertion dans MongoDB

---
+++ src/Document/Horaire.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+#[ODM\Document(collection: 'horaires')]
+class Horaire
+{
+    #[ODM\Id]
+    private ?string $id = null;
+
+    #[ODM\Field(type: 'string')]
+    private string $jour;
+
+    #[ODM\Field(type: 'string')]
+    private string $heureOuverture;
+
+    #[ODM\Field(type: 'string')]
+    private string $heureFermeture;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getJour(): string
+    {
+        return $this->jour;
+    }
+
+    public function setJour(string $jour): self
+    {
+        $this->jour = $jour;
+        return $this;
+    }
+
+    public function getHeureOuverture(): string
+    {
+        return $this->heureOuverture;
+    }
+
+    public function setHeureOuverture(string $heureOuverture): self
+    {
+        $this->heureOuverture = $heureOuverture;
+        return $this;
+    }
+
+    public function getHeureFermeture(): string
+    {
+        return $this->heureFermeture;
+    }
+
+    public function setHeureFermeture(string $heureFermeture): self
+    {
+        $this->heureFermeture = $heureFermeture;
+        return $this;
+    }
+}
+
+
