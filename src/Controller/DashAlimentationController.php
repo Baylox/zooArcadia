51c1cdd2709e0abe396679cbcfab8bb37e374f90
Baylospawn <microsoftbaylo@laposte.net>
2024-11-13 22:40:46 +0100

    Ajout de contrôles d'accès basés sur le rôle ROLE_EMPLOYE dans le DashAlimentationController

--- src/Controller/DashAlimentationController.php
+++ src/Controller/DashAlimentationController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/dashboard/alimentation')]
+#[IsGranted('ROLE_EMPLOYE')]
 final class DashAlimentationController extends AbstractController
 {
     #[Route(name: 'dashboard_alimentation_index', methods: ['GET'])]
