22f4177d440c703871f25e8bb1dfacd88d4165f1
Baylospawn <microsoftbaylo@laposte.net>
2024-11-13 23:03:55 +0100

    Ajout de contrôles d'accès basés sur le rôle ROLE_EMPLOYE dans le Dashbaord

--- src/Controller/DashEmployeController.php
+++ src/Controller/DashEmployeController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_EMPLOYE')]
 class DashEmployeController extends AbstractController
-{
+{   
     #[Route('/dash/employe', name: 'app_dash_employe')]
     public function index(): Response
     {
