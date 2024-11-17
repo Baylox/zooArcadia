589a00cffd2b9b62712ba11ddd16190e58ca39b7
Baylospawn <microsoftbaylo@laposte.net>
2024-11-17 05:40:21 +0100

    Ajout d'un formulaire de contact avec validation et envoi d'email, et mise à jour de la navigation

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            // Envoyer un email au zoo
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('zoo@arcadia.fr') // Adresse email du zoo
+                ->subject($data['titre'])
+                ->text($data['description']);
+            
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été envoyé avec succès.');
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('pages/contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
