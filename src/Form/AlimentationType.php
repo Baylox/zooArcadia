742e930c52d180461d013723429cd808e7836d0d
Baylospawn <microsoftbaylo@laposte.net>
2024-11-18 01:17:21 +0100

    Refactorisation du formulaire d'Alimentation : Suppression de la possibilité de mettre une quantité dans l'alimentation déstiné préalablement uniquement aux rapports

--- src/Form/AlimentationType.php
+++ src/Form/AlimentationType.php
@@ -3,24 +3,27 @@
 namespace App\Form;
 
 use App\Entity\Alimentation;
-use App\Entity\Rapport;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 
 class AlimentationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomNourriture')
-            ->add('quantiteNourriture', NumberType::class, [
-                'label' => 'Quantité de Nourriture',
-                'scale' => 2, // Précision pour les décimales
-            ])
-            ->add('commentaireVeterinaire');
+        ->add('nomNourriture', TextType::class, [
+            'label' => 'Nom de la Nourriture',
+            'attr' => ['class' => 'form-control'],
+        ])
+        ->add('commentaireVeterinaire', TextareaType::class, [
+            'label' => 'Commentaire du Vétérinaire',
+            'attr' => ['class' => 'form-control', 'rows' => 3],
+            'required' => false,
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
