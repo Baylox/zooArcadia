0903e8db82e91f5787153193ed3a2f6e0b8983e2
Baylospawn <microsoftbaylo@laposte.net>
2025-03-20 00:48:08 +0100

    Réduction de la longueur de la colonne 'nom' dans l'entité Habitat

--- src/Entity/Habitat.php
+++ src/Entity/Habitat.php
@@ -16,7 +16,7 @@ class Habitat
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 150)]
+    #[ORM\Column(length: 50)]
     private ?string $nom = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
