9eb34ff9056964dfe2ec2f8722de60b9cb09ba3d
Baylospawn <microsoftbaylo@laposte.net>
2025-03-20 00:50:36 +0100

    Réduction de la longueur de la colonne 'prenom' dans l'entité Animal

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -16,7 +16,7 @@ class Animal
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(length: 50)]
     private ?string $prenom = null;
 
     #[ORM\Column(length: 50)]
